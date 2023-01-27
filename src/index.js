e1f180b1ba74350ec0a78a37751edb24c676af81
johngaalt <rajon777666@gmail.com>
2023-01-27 12:25:33 +0300

    fix: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(num) {
+    let ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    let tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    if (num === 0) {
+        return "zero";
+    } else if (num < 20) {
+        return ones[num];
+    } else if (num < 100) {
+        return (
+            tens[Math.floor(num / 10)] +
+            (num % 10 !== 0 ? " " + ones[num % 10] : "")
+        );
+    } else {
+        return (
+            ones[Math.floor(num / 100)] +
+            " hundred" +
+            (num % 100 !== 0 ? " " + toReadable(num % 100) : "")
+        );
+    }
+};
